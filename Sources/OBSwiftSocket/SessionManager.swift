da9dfa1c63a532115fd450f0b55dc86d4fa24196
edonv <22782929+edonv@users.noreply.github.com>
2022-07-15 10:52:34 -0400

    Update SessionManager.swift
    Reordered `sendRequestBatch` functions

--- Sources/OBSwiftSocket/SessionManager.swift
+++ Sources/OBSwiftSocket/SessionManager.swift
@@ -188,6 +188,23 @@ public extension OBSSessionManager {
             .eraseToAnyPublisher()
     }
     
+    // TODO: Create docs here. Note that not all provided requests have to be the same kind.
+    // Instead, it's an array of pre-wrapped Request messages.
+    func sendRequestBatch(executionType: OBSEnums.RequestBatchExecutionType? = .serialRealtime,
+                          requests: [OpDataTypes.RequestBatch.Request?]) throws -> AnyPublisher<[String: OBSRequestResponse], Error> {
+        try checkForConnection()
+        
+        let msgBodyToSend = OpDataTypes.RequestBatch(id: UUID().uuidString, executionType: executionType, requests: requests.compactMap { $0 })
+        let msgToSend = Message<OpDataTypes.RequestBatch>(data: msgBodyToSend)
+        
+        return wsPublisher.send(msgToSend)
+            .flatMap { self.publisher(forAllMessagesOfType: OpDataTypes.RequestBatchResponse.self) }
+            .filter { [msgBodyToSend] receivedMsgBody in receivedMsgBody.id == msgBodyToSend.id }
+            .first() // Finishes the stream after allowing 1 of the correct type through
+            .tryMap { try $0.mapResults() }
+            .eraseToAnyPublisher()
+    }
+    
     /// <#Description#>
     /// - Parameters:
     ///   - executionType: <#executionType description#>
@@ -209,23 +226,6 @@ public extension OBSSessionManager {
             .map { respDict in
                 respDict.compactMapValues { $0 as? R.ResponseType }
             }
-            .eraseToAnyPublisher()
-    }
-    
-    // TODO: Create docs here. Note that not all provided requests have to be the same kind.
-    // Instead, it's an array of pre-wrapped Request messages.
-    func sendRequestBatch(executionType: OBSEnums.RequestBatchExecutionType? = .serialRealtime,
-                          requests: [OpDataTypes.RequestBatch.Request?]) throws -> AnyPublisher<[String: OBSRequestResponse], Error> {
-        try checkForConnection()
-        
-        let msgBodyToSend = OpDataTypes.RequestBatch(id: UUID().uuidString, executionType: executionType, requests: requests.compactMap { $0 })
-        let msgToSend = Message<OpDataTypes.RequestBatch>(data: msgBodyToSend)
-        
-        return wsPublisher.send(msgToSend)
-            .flatMap { self.publisher(forAllMessagesOfType: OpDataTypes.RequestBatchResponse.self) }
-            .filter { [msgBodyToSend] receivedMsgBody in receivedMsgBody.id == msgBodyToSend.id }
-            .first() // Finishes the stream after allowing 1 of the correct type through
-            .tryMap { try $0.mapResults() }
             .eraseToAnyPublisher()
     }
 }
