5164b1689fafd382adf5f28be9afb318dd7fd21e
edonv <22782929+edonv@users.noreply.github.com>
2022-07-29 14:08:11 -0400

    Update SessionManager Extensions.swift
    Added a new specialized publisher that publishes the current list of scenes. It publishes any changes as well.

--- Sources/OBSwiftSocket/Extensions/SessionManager Extensions.swift
+++ Sources/OBSwiftSocket/Extensions/SessionManager Extensions.swift
@@ -50,4 +50,33 @@ extension OBSSessionManager {
             .map { $0 as SceneNamePair }
             .eraseToAnyPublisher()
     }
+    
+    /// Creates a `Publisher` that returns the current scene list, updating with any changes.
+    /// - Throws: `WebSocketPublisher.WSErrors.noActiveConnection` error if there isn't an active connection.
+    /// Thrown by `checkForConnection()`.
+    /// - Returns: A `Publisher` containing the scene list that re-publishes every time the list changes.
+    func sceneListPublisher() throws -> AnyPublisher<[OBSRequests.Subtypes.Scene], Error> {
+        // Get initial value
+        let getCurrentSceneList = try sendRequest(OBSRequests.GetSceneList())
+        
+        // Listen for updates to scene list
+        let sceneCreatedListener = try listenForEvent(OBSEvents.SceneCreated.self, firstOnly: false)
+            .ignoreOutput()
+        let sceneRemovedListener = try listenForEvent(OBSEvents.SceneRemoved.self, firstOnly: false)
+            .ignoreOutput()
+        let sceneNameChangedListener = try listenForEvent(OBSEvents.SceneNameChanged.self, firstOnly: false)
+            .ignoreOutput()
+        let sceneListChanedListener = try listenForEvent(OBSEvents.SceneListChanged.self, firstOnly: false)
+            .ignoreOutput()
+        
+        let eventListener = Publishers.Merge4(sceneCreatedListener,
+                                              sceneRemovedListener,
+                                              sceneNameChangedListener,
+                                              sceneListChanedListener)
+            .tryFlatMap { _ in try self.sendRequest(OBSRequests.GetSceneList()) }
+        
+        return Publishers.Merge(getCurrentSceneList, eventListener)
+            .tryMap { try $0.typedScenes() }
+            .eraseToAnyPublisher()
+    }
 }
