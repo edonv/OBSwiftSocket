f02981f26fa302796b4dba1c888d17ec17029553
edonv <22782929+edonv@users.noreply.github.com>
2022-07-29 13:56:25 -0400

    Create SessionManager Extensions.swift
    Created a new file and extension for housing specialized publishers and their logic.

---
+++ Sources/OBSwiftSocket/Extensions/SessionManager Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  SessionManager Extensions.swift
+//  
+//
+//  Created by Edon Valdman on 7/29/22.
+//
+
+import Foundation
+import Combine
+import JSONValue
+
+extension OBSSessionManager {
+}
