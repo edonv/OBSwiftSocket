ef15a51c83a9b206a286ff6c4a5f312b75f8bb97
edonv <22782929+edonv@users.noreply.github.com>
2022-07-31 20:46:28 -0400

    Update WSPublisher Extensions.swift
    Added option for sending messages deferred to `Encodable` message-sending function.

--- Sources/OBSwiftSocket/Extensions/WSPublisher Extensions.swift
+++ Sources/OBSwiftSocket/Extensions/WSPublisher Extensions.swift
@@ -18,6 +18,7 @@ extension WebSocketPublisher {
     /// - Throws: `WSErrors.noActiveConnection` if there isn't an active connection.
     /// - Returns: A `Publisher` without any value, signalling the message has been sent.
     func send<T: Encodable>(_ message: T,
+                            deferred: Bool,
                             encodingMode: OBSSessionManager.ConnectionData.MessageEncoding) throws -> AnyPublisher<Void, Error> {
         switch encodingMode {
         case .json:
@@ -25,14 +26,14 @@ extension WebSocketPublisher {
                 return Fail(error: CodingErrors.failedToEncodeObject(.json))
                     .eraseToAnyPublisher()
             }
-            return try send(json)
+            return try send(json, deferred: deferred)
         
         case .msgPack:
             guard let msgData = try? MessagePackEncoder().encode(message) else {
                 return Fail(error: CodingErrors.failedToEncodeObject(.msgPack))
                     .eraseToAnyPublisher()
             }
-            return try send(msgData)
+            return try send(msgData, deferred: deferred)
         }
     }
 }
