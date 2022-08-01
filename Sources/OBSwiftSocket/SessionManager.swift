ff7824b54a874e8c31a04da06e03b491e92780fd
edonv <22782929+edonv@users.noreply.github.com>
2022-07-31 20:47:41 -0400

    Update SessionManager.swift
    Added option for sending messages deferred.

--- Sources/OBSwiftSocket/SessionManager.swift
+++ Sources/OBSwiftSocket/SessionManager.swift
@@ -208,9 +208,9 @@ extension OBSSessionManager {
     /// - Parameter body: The data that should be wrapped in a `Message<Body>` and sent.
     /// - Throws: `WebSocketPublisher.WSErrors.noActiveConnection` error if there isn't an active connection.
     /// - Returns: A `Publisher` without any value, signalling that the message has been sent.
-    public func sendMessage<Body: OBSOpData>(_ body: Body) throws -> AnyPublisher<Void, Error> {
+    public func sendMessage<Body: OBSOpData>(_ body: Body, deferred: Bool = false) throws -> AnyPublisher<Void, Error> {
         let msg = Message<Body>(data: body)
-        return try self.wsPublisher.send(msg, encodingMode: self.encodingProtocol)
+        return try self.wsPublisher.send(msg, deferred: deferred, encodingMode: self.encodingProtocol)
     }
     
     /// Sends a `Request` message wrapped around the given `OBSRequest` body.
