2a3571a33b92ea33ad61a974fc3a6e93740ba8aa
edonv <22782929+edonv@users.noreply.github.com>
2022-08-06 15:55:15 -0400

    Update Custom Combine Publishers.swift
    Made `ReplaySubject` and associated types `public` and restructured code.

--- Sources/OBSwiftSocket/Extensions/Custom Combine Publishers.swift
+++ Sources/OBSwiftSocket/Extensions/Custom Combine Publishers.swift
@@ -14,7 +14,7 @@ extension Publisher {
     /// Provides a subject that shares a single subscription to the upstream publisher and
     /// replays at most `bufferSize` items emitted by that publisher.
     /// - Parameter bufferSize: limits the number of items that can be replayed.
-    func shareReplay(_ bufferSize: Int) -> AnyPublisher<Output, Failure> {
+    public func shareReplay(_ bufferSize: Int) -> AnyPublisher<Output, Failure> {
         return multicast(subject: Publishers.ReplaySubject(bufferSize))
             .autoconnect()
             .eraseToAnyPublisher()
@@ -22,85 +22,83 @@ extension Publisher {
 }
 
 extension Publishers {
-    final class ReplaySubject<Output, Failure: Error>: Subject {
+    public final class ReplaySubject<Output, Failure: Error>: Subject {
         private var buffer = [Output]()
         private let bufferSize: Int
-        private let lock = NSRecursiveLock()
-        
         private var subscriptions = [Subscriptions.ReplaySubject<Output, Failure>]()
         private var completion: Subscribers.Completion<Failure>?
+        private let lock = NSRecursiveLock()
         
-        init(_ bufferSize: Int = 0) {
+        public init(_ bufferSize: Int = 0) {
             self.bufferSize = bufferSize
         }
-    }
-}
-
-extension Publishers.ReplaySubject {
-    func receive<Downstream: Subscriber>(subscriber: Downstream) where Downstream.Failure == Failure, Downstream.Input == Output {
-        lock.lock(); defer { lock.unlock() }
-        let subscription = Subscriptions.ReplaySubject<Output, Failure>(downstream: AnySubscriber(subscriber))
-        subscriber.receive(subscription: subscription)
-        subscriptions.append(subscription)
-        subscription.replay(buffer, completion: completion)
-    }
-}
-
-extension Publishers.ReplaySubject {
-    /// Establishes demand for a new upstream subscriptions
-    func send(subscription: Subscription) {
-        lock.lock(); defer { lock.unlock() }
-        subscription.request(.unlimited)
-    }
-    
-    /// Sends a value to the subscriber.
-    func send(_ value: Output) {
-        lock.lock(); defer { lock.unlock() }
-        buffer.append(value)
-        buffer = buffer.suffix(bufferSize)
-        subscriptions.forEach { $0.receive(value) }
-    }
-    
-    /// Sends a completion event to the subscriber.
-    func send(completion: Subscribers.Completion<Failure>) {
-        lock.lock(); defer { lock.unlock() }
-        self.completion = completion
-        subscriptions.forEach { subscription in subscription.receive(completion: completion) }
+        
+        /// Provides this Subject an opportunity to establish demand for any new upstream subscriptions
+        public func send(subscription: Subscription) {
+            lock.lock(); defer { lock.unlock() }
+            subscription.request(.unlimited)
+        }
+        
+        /// Sends a value to the subscriber.
+        public func send(_ value: Output) {
+            lock.lock(); defer { lock.unlock() }
+            buffer.append(value)
+            buffer = buffer.suffix(bufferSize)
+            subscriptions.forEach { $0.receive(value) }
+        }
+        
+        /// Sends a completion signal to the subscriber.
+        public func send(completion: Subscribers.Completion<Failure>) {
+            lock.lock(); defer { lock.unlock() }
+            self.completion = completion
+            subscriptions.forEach { subscription in subscription.receive(completion: completion) }
+        }
+        
+        /// This function is called to attach the specified `Subscriber` to the`Publisher
+        public func receive<Downstream: Subscriber>(subscriber: Downstream) where Downstream.Failure == Failure, Downstream.Input == Output {
+            lock.lock(); defer { lock.unlock() }
+            let subscription = Subscriptions.ReplaySubject<Output, Failure>(downstream: AnySubscriber(subscriber))
+            subscriber.receive(subscription: subscription)
+            subscriptions.append(subscription)
+            subscription.replay(buffer, completion: completion)
+        }
     }
 }
 
 extension Subscriptions {
-    final class ReplaySubject<Output, Failure: Error>: Subscription {
+    /// A class representing the connection of a subscriber to a publisher.
+    public final class ReplaySubject<Output, Failure: Error>: Subscription {
         private let downstream: AnySubscriber<Output, Failure>
         private var isCompleted = false
         private var demand: Subscribers.Demand = .none
         
-        init(downstream: AnySubscriber<Output, Failure>) {
+        public init(downstream: AnySubscriber<Output, Failure>) {
             self.downstream = downstream
         }
         
-        func request(_ newDemand: Subscribers.Demand) {
+        // Tells a publisher that it may send more values to the subscriber.
+        public func request(_ newDemand: Subscribers.Demand) {
             demand += newDemand
         }
         
-        func cancel() {
+        public func cancel() {
             isCompleted = true
         }
         
-        func receive(_ value: Output) {
+        public func receive(_ value: Output) {
             guard !isCompleted, demand > 0 else { return }
             
             demand += downstream.receive(value)
             demand -= 1
         }
         
-        func receive(completion: Subscribers.Completion<Failure>) {
+        public func receive(completion: Subscribers.Completion<Failure>) {
             guard !isCompleted else { return }
             isCompleted = true
             downstream.receive(completion: completion)
         }
         
-        func replay(_ values: [Output], completion: Subscribers.Completion<Failure>?) {
+        public func replay(_ values: [Output], completion: Subscribers.Completion<Failure>?) {
             guard !isCompleted else { return }
             values.forEach { value in receive(value) }
             if let completion = completion { receive(completion: completion) }
