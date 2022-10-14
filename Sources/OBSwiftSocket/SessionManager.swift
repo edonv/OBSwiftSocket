b5709bbe6928076d8a944293626cac1a5de8bf20
Edon <edon@valdman.works>
2022-10-14 06:58:45 -0400

    Update SessionManager.swift
    Updated the documentation

--- Sources/OBSwiftSocket/SessionManager.swift
+++ Sources/OBSwiftSocket/SessionManager.swift
@@ -162,7 +162,8 @@ extension OBSSessionManager {
     /// Connects to OBS using `connectionData`.
     /// - Parameters:
     ///   - persistConnectionData: Whether `connectionData` should be persisted if connected successfully.
-    ///   - events: Bit mask (`OptionSet`) of which `OBSEvents` to be alerted of.
+    ///   - events: Bit mask (`OptionSet`) of which `OBSEvents` to be alerted of. If `nil`, all normal
+    ///   events are subscribed to.
     /// - Throws: Can throw `Errors.noConnectionData` if there is no connectionData set.
     /// Can also throw `Errors.alreadyConnected` if `wsPublisher` is already running.
     /// - Returns: A `Publisher` that completed upon connecting successfully. If connection process fails,
