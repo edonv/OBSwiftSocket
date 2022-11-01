0dce3c3da77ff37232dc3da722e856c14ab17a46
Edon <edon@valdman.works>
2022-11-01 10:43:32 -0400

    Update Package.swift
    Removed boilerplate comments

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -10,13 +9,11 @@ let package = Package(
         .macOS(SupportedPlatform.MacOSVersion.v10_15)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "OBSwiftSocket",
             targets: ["OBSwiftSocket"]),
     ],
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
 //        .package(
 //            name: "swift-format",
 //            url: "https://github.com/apple/swift-format.git",
@@ -33,8 +30,6 @@ let package = Package(
                  branch: "main"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OBSwiftSocket",
             dependencies: [
