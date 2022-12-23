00d2d98c3fccbbe5fd0efdf31bb39c7a2d78741e
Edon <edon@valdman.works>
2022-12-23 12:14:52 -0500

    Update OBSwiftSocketTests.swift
    Removed default tests

--- Tests/OBSwiftSocketTests/OBSwiftSocketTests.swift
+++ Tests/OBSwiftSocketTests/OBSwiftSocketTests.swift
@@ -1,15 +1,7 @@
 import XCTest
-@testable import OBSwiftSocket
+import OBSwiftSocket
 
 final class OBSwiftSocketTests: XCTestCase {
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(OBSwiftSocket().text, "Hello, World!")
-    }
-
     static var allTests = [
-        ("testExample", testExample),
     ]
 }
